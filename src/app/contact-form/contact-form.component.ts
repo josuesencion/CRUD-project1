241f1d2de307a56b05f836aa8afbb725ca14f224
josuesencion <josue.sencion1@gmail.com>
2019-06-01 13:39:47 -0400

    form that submits records and stores them locallly, sort of ...

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm} from '@angular/forms';
+import { timingSafeEqual } from 'crypto';
+
+class Contact{
+  id: number;
+  name: string;
+  lastName: string;
+  phone: string;
+}
 
 @Component({
   selector: 'app-contact-form',
@@ -6,12 +15,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-  id: number;
-  firstName: string;
-  lastName: string;
-  phone: string;
+  private currContact: Contact = new Contact();
+  private id: number=0;
+  private contacts: Contact[] = [];
 
   constructor() { }
+
+  onSubmit(form: NgForm){
+    this.contacts.push({...form.value, id: this.id++});
+    // form.reset();
+    console.log(this.contacts);
+  }
 
   ngOnInit() {
     console.log("hmmm... what is this?")
