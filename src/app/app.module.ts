b886cf96372702eb2fe2b746cdd4387782531acb
Josue Sencion <josue.sencion1@gmail.com>
2019-06-04 13:53:22 -0400

    deleted some test modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { PhoneComponent } from './phone/phone.component';
 import { ContactComponent } from './contact/contact.component';
-import { UserComponent } from './user/user.component';
-import { CustomerComponent } from './customer/customer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactFormComponent,
-    PhoneComponent,
     ContactComponent,
-    UserComponent,
-    CustomerComponent
   ],
   imports: [
     BrowserModule,
